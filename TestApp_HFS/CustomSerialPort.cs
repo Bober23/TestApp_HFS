af38c171d6acb82d95eaa909c23e7574666f286e
Bober23 <vanya.popov.2003@gmail.com>
2023-08-31 13:01:21 +0300

    Добавьте файлы проекта.

---
+++ TestApp_HFS/CustomSerialPort.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+
+namespace TestApp_HFS
+{
+     class CustomSerialPort:SerialPort
+     {
+        private const int DataSize = 1024;    //  я так и не понял, какой размер данных нужен. Укажите правильное число в байтах
+        private byte[] _bufer = new byte[DataSize];
+        public CustomSerialPort(string port)
+            : base()
+        {
+            base.PortName = port;
+            base.BaudRate = 9600;
+            base.DataBits = 8;
+            base.StopBits = StopBits.One;
+            base.Parity = Parity.None;
+            base.ReadTimeout = 1000;
+
+            base.DataReceived += SerialPort_DataReceived;
+        }
+        //  открываем порт передав туда имя
+        public void Open(string portName)
+        {
+            if (base.IsOpen)
+            {
+                base.Close();
+            }
+            base.PortName = portName;
+            base.Open();
+        }
+        public void Print(byte[] massive)
+        {
+            foreach (byte b in massive)
+            {
+                Console.Write(b);
+            }
+
+        }
+        void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var port = (SerialPort)sender;
+            try
+            {
+                if (port.BytesToRead > 0)
+                {
+                    byte[] answer = new byte[(int)port.BytesToRead];
+                    port.Read(answer, 0, port.BytesToRead);
+                    Print(answer);
+                }
+            }
+            catch { }
+            
+        }
+    }
+}
---
+++ TestApp_HFS/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO.Ports;
+using TestApp_HFS;
+Console.WriteLine("Cписок доступных портов:");
+string[] ports = SerialPort.GetPortNames();
+string portName = null;
+foreach (string availablePort in ports)
+{
+    Console.WriteLine(availablePort);
+}
+Console.WriteLine("Введите порт");
+portName = Console.ReadLine();
+while (ports.All(x => x != portName))
+{
+    Console.WriteLine("Неправильный порт");
+    Console.WriteLine("Введите порт");
+    portName = Console.ReadLine();
+}
+
+Console.WriteLine($"Чтобы начать чтение данных из порта {portName} нажмите Enter");
+if (Console.ReadLine() == String.Empty)
+{
+    var port = new CustomSerialPort(portName);
+    port.Open(portName);
+}
+while (true)
+{
+    //Отправка данных в NATS
+
+}
